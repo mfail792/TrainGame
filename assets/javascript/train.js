4137993923312e4a58c255092bea5b23a2630ad8
Magus <mfail792@gmail.com>
2019-08-09 20:43:54 -0500

    added jquery link, JS link, firebase config

---
+++ assets/javascript/train.js
@@ -0,0 +1,11 @@
+// referencing firebase server to authenicate and initialize the application
+const firebaseConfig = {
+    apiKey: "AIzaSyBI99NPnup0E664hUhummmuSYKwX0p_UBA",
+    authDomain: "project1-ebc4f.firebaseapp.com",
+    databaseURL: "https://project1-ebc4f.firebaseio.com",
+    projectId: "project1-ebc4f",
+    storageBucket: "",
+    messagingSenderId: "677210244714",
+    appId: "1:677210244714:web:e074b7c2ed781fc8"
+};
+
